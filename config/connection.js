b6d83cec9e5dc14e05207d46048139a691c327d6
Dimitar Micevski <dmicevski@hotmail.com>
2020-12-29 10:49:13 -0500

    updated readme and npm installed console.table

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
+  port: 3306,
   // Your username
   user: "root",
   // Your password
--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,12 @@ USE employees;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,4) NULL,
   department_id INT NULL,
   FOREIGN KEY(department_id) REFERENCES department(id) on DELETE CASCADE
@@ -18,8 +18,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NOT NULL,
   FOREIGN KEY(role_id) REFERENCES role(id) on DELETE CASCADE,
